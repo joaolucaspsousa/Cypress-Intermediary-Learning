60c6bab1bb511236210e011bb06e44dcb673c255
João Lucas Pereira <j.lucaspereira@usp.br>
2023-03-08 15:32:24 -0300

    created a session save

--- cypress/e2e/GUI/login.cy.js
+++ cypress/e2e/GUI/login.cy.js
@@ -1,5 +1,5 @@
 describe('Login in Application', () => {
     it('Login via GUI', () => {
-        cy.login()
+        cy.login(Cypress.env('username'), Cypress.env('password'), { cacheSession: false })
     })
 })
